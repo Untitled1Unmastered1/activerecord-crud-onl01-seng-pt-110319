8d24d5a26b3995287952597b9fca0554e7f1e18a
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-02-23 04:26:24 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -95,7 +95,7 @@ end
 
 def can_destroy_a_single_item
   movie = Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-  Movie.delete 
+  movie.destroy 
 end
 
 def can_destroy_all_items_at_once
